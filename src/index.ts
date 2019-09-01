bbea9e36d6a94450c6f81e20ace254bc15f3b466
Phil Lee <phil@authvia.com>
2019-09-01 16:32:23 -0700

    Exporting 'sqsListeners' decorator from module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { sqsListener } from './sqsListener';
 export { SQSRouter } from './SQSRouter';
 export { SQS, MessageReceipt, SQSBodySerializer } from './SQS';
+export { sqsListeners } from './sqsListeners';
